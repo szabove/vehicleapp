5678117061086d79844205f895eeda166577b12f
szabove <szabove@gmail.com>
2020-03-24 18:16:04 +0100

    Migrations

---
+++ VehicleApp.DAL/Migrations/202003241523401_NavigationPropertyVehicleModelsMarkedAsVirtual.cs
@@ -0,0 +1,26 @@
+﻿namespace VehicleApp.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NavigationPropertyVehicleModelsMarkedAsVirtual : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.VehicleModel", "VehicleMake_VehicleMakelId", "dbo.VehicleMake");
+            DropIndex("dbo.VehicleModel", new[] { "VehicleMake_VehicleMakelId" });
+            AlterColumn("dbo.VehicleModel", "VehicleMake_VehicleMakelId", c => c.Guid(nullable: false));
+            CreateIndex("dbo.VehicleModel", "VehicleMake_VehicleMakelId");
+            AddForeignKey("dbo.VehicleModel", "VehicleMake_VehicleMakelId", "dbo.VehicleMake", "VehicleMakelId", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.VehicleModel", "VehicleMake_VehicleMakelId", "dbo.VehicleMake");
+            DropIndex("dbo.VehicleModel", new[] { "VehicleMake_VehicleMakelId" });
+            AlterColumn("dbo.VehicleModel", "VehicleMake_VehicleMakelId", c => c.Guid());
+            CreateIndex("dbo.VehicleModel", "VehicleMake_VehicleMakelId");
+            AddForeignKey("dbo.VehicleModel", "VehicleMake_VehicleMakelId", "dbo.VehicleMake", "VehicleMakelId");
+        }
+    }
+}
---
+++ VehicleApp.DAL/Migrations/202003241525224_GuidFix.cs
@@ -0,0 +1,16 @@
+﻿namespace VehicleApp.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GuidFix : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
