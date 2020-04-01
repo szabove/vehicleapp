cb70af6ba2bbedf924d77b384ce4198c3d5b41ba
szabove <szabove@gmail.com>
2020-04-01 09:19:46 +0200

    Created RestModels in WebApi project

---
+++ VehicleApp.WebApi/ViewModels/VehicleMakeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleApp.WebApi.ViewModels
+{
+    public class VehicleMakeViewModel
+    {
+        public Guid VehicleMakeId { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+        public ICollection<VehicleModelViewModel> VehicleModel { get; set; }
+    }
+}
---
+++ VehicleApp.WebApi/ViewModels/VehicleModelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleApp.WebApi.ViewModels
+{
+    public class VehicleModelViewModel
+    {
+        public Guid VehicleMakeId { get; set; }
+        public Guid VehicleModelId { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+        public VehicleMakeViewModel VehicleMake { get; set; }
+    }
+}
