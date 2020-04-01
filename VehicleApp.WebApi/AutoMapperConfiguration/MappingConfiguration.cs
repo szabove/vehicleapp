a303abac193f2d4c04407dec1887d3daf2c6a35d
szabove <szabove@gmail.com>
2020-04-01 09:26:03 +0200

    Created MappingConfiguration to include all mapping classes which inherit from Profile class, by assembly scanning

---
+++ VehicleApp.WebApi/AutoMapperConfiguration/MappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleApp.WebApi.AutoMapperConfiguration
+{
+    static public class MappingConfiguration
+    {
+        public static void IncludeAllMappingProfiles()
+        {
+            var config = new MapperConfiguration(cfg =>
+                cfg.AddMaps(new[] {
+                    typeof(VehicleApp.WebApi.AutoMapperConfiguration.RestToDomainModelMapping)
+                })
+            );
+        }
+    }
+}
