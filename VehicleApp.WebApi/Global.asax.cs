45a3154649a1b253bfdd46c37ac07238484b5cf0
szabove <szabove@gmail.com>
2020-04-01 09:27:03 +0200

    Run MappingConfiguration.IncludeAllMappingProfiles function at the application start

--- VehicleApp.WebApi/Global.asax.cs
+++ VehicleApp.WebApi/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using VehicleApp.WebApi.AutoMapperConfiguration;
 
 namespace VehicleApp.WebApi
 {
@@ -13,6 +14,9 @@ namespace VehicleApp.WebApi
     {
         protected void Application_Start()
         {
+
+            MappingConfiguration.IncludeAllMappingProfiles();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -21,3 +25,4 @@ namespace VehicleApp.WebApi
         }
     }
 }
+ 
