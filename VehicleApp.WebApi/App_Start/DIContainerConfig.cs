a9d77864e8acfd9da136daa63bb3304cd699b393
szabove <szabove@gmail.com>
2020-04-04 13:51:12 +0200

    Unnecessary code from ValueController and a file deleted Referenced Model.Common class library to WebApi project

---
+++ VehicleApp.WebApi/App_Start/DIContainerConfig.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleApp.WebApi.App_Start
+{
+    public static class DIContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            
+
+            //builder.RegisterType<VehicleMakeService>().As<Services.Common.IVehicleMakeService>();
+
+            
+
+            return builder.Build();
+        }
+    }
+}
--- VehicleApp.WebApi/Controllers/ValuesController.cs
+++ VehicleApp.WebApi/Controllers/ValuesController.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using VehicleApp.DAL;
 
 namespace VehicleApp.WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+
+
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -20,26 +22,8 @@ namespace VehicleApp.WebApi.Controllers
         // GET api/values/5
         public void Get(int id)
         {
-            using (var ctx = new VehicleContext())
-            {
-                //var x = new VehicleMake()
-                //{
-                //    Name = "Opel",
-                //    Abrv = "Corsa D"
-                //};
 
-                //ctx.VehicleMake.Add(x);
-                //ctx.SaveChanges();
-
-                Debug.Print(ctx.VehicleMake.First().Name);
-
-            }
         }
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
 
         // POST api/values
         public void Post([FromBody]string value)
