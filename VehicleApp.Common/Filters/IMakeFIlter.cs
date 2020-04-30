327dc6a29b9bc512864ba2111f1d682563f8828d
szabove <szabove@gmail.com>
2020-04-30 11:57:29 +0200

    Created filter class for vehiclemake

---
+++ VehicleApp.Common/Filters/IMakeFIlter.cs
@@ -0,0 +1,18 @@
+﻿using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Model;
+using VehicleApp.Model.Common;
+
+namespace VehicleApp.Common
+{
+    public interface IMakeFilter
+    {
+        string Search { get; set; }
+        Expression<Func<IVehicleMake, bool>> GetFilterQuery();
+    }
+}
---
+++ VehicleApp.Common/Filters/MakeFIlter.cs
@@ -0,0 +1,42 @@
+﻿using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleApp.Model;
+using VehicleApp.Model.Common;
+
+namespace VehicleApp.Common
+{
+    public class MakeFilter : IMakeFilter
+    {
+        public MakeFilter()
+        {
+        }
+
+        public string Search { get; set; }
+
+        public Expression<Func<IVehicleMake, bool>> GetFilterQuery()
+        {
+            var predicate = PredicateBuilder.New<IVehicleMake>(true);
+
+            if (!string.IsNullOrEmpty(Search))
+            {
+                predicate = FilterByName(predicate);
+            }
+
+            return predicate;
+        }
+        
+        //Filtering Functions
+        ExpressionStarter<IVehicleMake> FilterByName(ExpressionStarter<IVehicleMake> predicate)
+        {
+            Search.ToLower();
+            return predicate = predicate.Start(name => name.Name.ToLower().Contains(Search));
+        }
+
+    }
+}
