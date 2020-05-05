71f0d43fffee796fc71708165560752570a4efe5
szabove <szabove@gmail.com>
2020-05-05 10:53:05 +0200

    Registering types in a DI module on common layer

--- VehicleApp.Common/CommonLayerDependency.cs
+++ VehicleApp.Common/CommonLayerDependency.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VehicleApp.Common.Filters;
 using VehicleApp.Model.Common;
 
 namespace VehicleApp.Common
@@ -13,7 +14,11 @@ namespace VehicleApp.Common
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<MakeFilter>().As<IMakeFilter>();
-            builder.RegisterType<VehicleMakePagination>().As<IPagination<IVehicleMake>>();
+            builder.RegisterType<ModelFilter>().As<IModelFilter>();
+            builder.RegisterType<MakePagination>().As<IPagination<IVehicleMake>>();
+            builder.RegisterType<ModelPagination>().As<IPagination<IVehicleModel>>(); 
+            builder.RegisterType<MakeSorter>().As<ISorter<IVehicleMake>>();
+            builder.RegisterType<ModelSorter>().As<ISorter<IVehicleModel>>();
         }
     }
 }
