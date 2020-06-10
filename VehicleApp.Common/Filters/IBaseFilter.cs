28518002c93b758017e9b24efd8560776ec6eb76
szabove <szabove@gmail.com>
2020-06-10 15:21:01 +0200

    BaseFilter interface created

---
+++ VehicleApp.Common/Filters/IBaseFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp.Common.Filters
+{
+    public interface IBaseFilter
+    {
+        string Search { get; set; }
+    }
+}
