cda31c9f848e81cbcbf11e4ad3b4117cfe61a260
szabove <szabove@gmail.com>
2020-06-19 15:54:47 +0200

    Removed unnecessary mappings

--- VehicleApp.WebApi/AutoMapperConfiguration/RestToDomainModelMapping.cs
+++ VehicleApp.WebApi/AutoMapperConfiguration/RestToDomainModelMapping.cs
@@ -7,7 +7,7 @@ using VehicleApp.Common;
 using VehicleApp.Common.Filters;
 using VehicleApp.Model;
 using VehicleApp.Model.Common;
-using VehicleApp.WebApi.ViewModels;
+using VehicleApp.WebApi.Controllers;
 
 namespace VehicleApp.WebApi.AutoMapperConfiguration
 {
@@ -15,19 +15,13 @@ namespace VehicleApp.WebApi.AutoMapperConfiguration
     {
         public RestToDomainModelMapping()
         {
-            //EntityMappings
             CreateMap<VehicleMake, MakeRest>().ReverseMap();
-            CreateMap<VehicleMake, IVehicleMake>().ReverseMap();
             CreateMap<IVehicleMake, MakeRest>().ReverseMap();
             CreateMap<ResponseCollection<IVehicleMake>, ResponseCollection<MakeRest>>().ReverseMap();
 
             CreateMap<VehicleModel, ModelRest>().ReverseMap();
-            CreateMap<VehicleModel, IVehicleModel>().ReverseMap();
             CreateMap<IVehicleModel, ModelRest>().ReverseMap();
             CreateMap<ResponseCollection<IVehicleModel>, ResponseCollection<ModelRest>>().ReverseMap();
-
-            ////FilterMappings
-            //CreateMap<BaseFilterParams, IBaseFilterParams>().ReverseMap();
         }
     }
 }
